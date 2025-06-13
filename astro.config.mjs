dfbbdb370c390186d1fd064226d90aab844213ae
Your Name <you@example.com>
2025-06-14 07:24:17 +0800

    查看gf-pages暂存

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
     output: 'static',
-    base: '/love7075.github.io/', // TODO: 将 'your-repo-name' 替换为你的 GitHub 仓库名
+    site: 'https://love7075.github.io',
+    base: '/', // TODO: 将 'your-repo-name' 替换为你的 GitHub 仓库名
     // adapter: vercel({
     //     webAnalytics: {
     //         enabled: true
